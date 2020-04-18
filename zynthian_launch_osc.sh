92dfe342264573b71f12e659f37c6fbf8b6d4ce6
jfrey <nomhad@gmail.com>
2020-04-19 00:14:04 +0200

    fixing submodules

--- zynthian_launch_osc.sh
+++ zynthian_launch_osc.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+export ZYNTHIN_RAISE_EXCEPTIONS=1
+
 cd zynthian-emuface && ./zynthian_oscface_full.py /home/jfrey/Bidouilles/zynthian/zynthian_gui_emu_osc.sh /home/jfrey/Bidouilles/zynthian/ /home/jfrey/Bidouilles/zynthian/zynthian-my-data/
